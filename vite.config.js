d36eea346966f5b4f5c048716708dc1d17e1c357
GuGuMur <2221533105@qq.com>
2024-10-27 18:22:24 +0800

    act: activate vercel

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import UnoCSS from 'unocss/vite'
 import vue from '@vitejs/plugin-vue'
 
+const host = process.env.TAURI_DEV_HOST;
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
@@ -17,18 +18,39 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
+  clearScreen: false,
   server: {
     proxy: {
-        '/ciba': {
-            target: 'https://open.iciba.com',
-            changeOrigin: true,
-            rewrite: (path) => path.replace(/^\/ciba/, '/dsapi/'),
-        },
-        '/shanbei': {
-          target: 'https://apiv3.shanbay.com',
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/shanbei/, '/'),
+      '/ciba': {
+        target: 'https://open.iciba.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/ciba/, '/dsapi/'),
+      },
+      '/shanbei': {
+        target: 'https://apiv3.shanbay.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/shanbei/, '/'),
       },
     },
-},
+    strictPort: true,
+    // if the host Tauri is expecting is set, use it
+    host: host || false,
+    port: 5173,
+    watch: {
+      // 3. tell vite to ignore watching `src-tauri`
+      ignored: ["**/src-tauri/**"],
+    },
+  },
+  envPrefix: ['VITE_', 'TAURI_ENV_*'],
+  build: {
+    // Tauri uses Chromium on Windows and WebKit on macOS and Linux
+    target:
+      process.env.TAURI_ENV_PLATFORM == 'windows'
+        ? 'chrome105'
+        : 'safari13',
+    // don't minify for debug builds
+    minify: !process.env.TAURI_ENV_DEBUG ? 'esbuild' : false,
+    // produce sourcemaps for debug builds
+    sourcemap: !!process.env.TAURI_ENV_DEBUG,
+  },
 })
